c49b3cba30b0cbfbefbc4380778a8612c0d37e93
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-06-04 13:27:05 -0400

    add Character type and getCharacters function to fetch from api

--- src/services/trek-api.ts
+++ src/services/trek-api.ts
@@ -1,1 +1,12 @@
+import { Character } from "../types/types";
 
+const URL = process.env.REACT_APP_API_URL as string;
+
+export const getCharacters = async(): Promise<Character[]> => {
+    const res = await fetch(URL);
+    const json = await res.json();
+
+    if(!res.ok) throw new Error('Uh oh, something went wrong');
+
+    return json;
+}
---
+++ src/types/types.ts
@@ -0,0 +1,8 @@
+export interface Character {
+    id: number;
+    name: string;
+    affiliation: string;
+    origin: string;
+    race: string;
+    imageUrl: string;
+}
