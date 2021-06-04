d899ee70cdcbbd346dee3400f5f2b1091894da29
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-06-04 13:17:50 -0400

    create CharacterItem component

--- src/components/characters/CharacterItem.tsx
+++ src/components/characters/CharacterItem.tsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './CharacterItem.module.css';
 
+const CharacterItem: React.FC = ({ id, name, imageUrl }) => {
+    return (
+        <div>
+            <Link 
+                to={`/detail/${id}`}
+                className={styles.link}
+            >
+                <figure>
+                    <img src={imageUrl} alt={name} className={styles.img} />
+                    <figcaption>{name}</figcaption>
+                </figure>
+            </Link>
+        </div>
+    )
+}
+
+export default CharacterItem;
